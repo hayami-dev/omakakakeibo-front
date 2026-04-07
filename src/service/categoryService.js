bd9578270b19804b0ff2bef7ef851713b71f25f9
sss <sss@sss.com>
2026-04-07 14:04:04 +0900

    INITIAL_CATEGORIESをcategoryServiceへ引っ越し

--- src/service/categoryService.js
+++ src/service/categoryService.js
@@ -1,4 +1,12 @@
-import { INITIAL_CATEGORIES } from "../categories";
+// 初期値
+export const INITIAL_CATEGORIES = [
+  { id: "c1", name: "必要経費", isActive: true, colorIndex: 0 },
+  { id: "c2", name: "ごほうび", isActive: true, colorIndex: 1 },
+  { id: "c3", name: "推し活", isActive: true, colorIndex: 2 },
+  { id: "c4", name: "カフェ", isActive: true, colorIndex: 3 },
+  { id: "c5", name: "わからない", isActive: true, colorIndex: 4 },
+  { id: "c6", name: "ああああああああああ", isActive: true, colorIndex: 5 },
+];
 
 const STORAGE_KEY = "myCategories";
 
