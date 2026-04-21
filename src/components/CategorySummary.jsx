3c28a8716cd939b97c0afbfda0a4bb1945d12ba1
sss <sss@sss.com>
2026-04-21 13:52:46 +0900

    CategorySummaryをカテゴリ名、色が出るよう修正

--- src/components/CategorySummary.jsx
+++ src/components/CategorySummary.jsx
@@ -1,4 +1,15 @@
+import { useAtom } from "jotai";
+import {
+  activeCategoriesAtom,
+  archivedCategoriesAtom,
+  resolveCategoryById,
+} from "../service/categoryService";
+
 export default function CategorySummary({ history }) {
+  const [activeCategories] = useAtom(activeCategoriesAtom);
+  const [archivedCategories] = useAtom(archivedCategoriesAtom);
+
+  // カテゴリid毎の金額の合計値を計算
   const categoryTotals = history.reduce(
     (acc, cur) => {
       const { categoryId, amount } = cur;
@@ -9,15 +20,41 @@ export default function CategorySummary({ history }) {
     {}, // 👈初期値
   );
 
+  // 合計値にカテゴリ名、色情報を付与
+  const summaryList = Object.entries(categoryTotals).map(
+    ([categoryId, sum]) => {
+      const category = resolveCategoryById(
+        categoryId,
+        activeCategories,
+        archivedCategories,
+      );
+      return {
+        id: categoryId,
+        sum: sum,
+        name: category?.name || "不明",
+        color: category?.style?.code || "gray",
+        colorIndex: category?.colorIndex ?? 999, // ソート用の重み
+      };
+    },
+  );
+
+  // 色順にソートする
+  const sortedCategory = summaryList.sort(
+    (a, b) => a.colorIndex - b.colorIndex,
+  );
+
   return (
     <>
       <h3>カテゴリ毎の集計</h3>
       <ul>
-        {Object.entries(categoryTotals).map(([cat, sum]) => (
-          <li key={cat}>
-            {cat}:{sum.toLocaleString()}円
-          </li>
-        ))}
+        {sortedCategory.map((item) => {
+          return (
+            <li key={item.id}>
+              <span style={{ color: item.color }}>●{item.name}</span>
+              {item.sum.toLocaleString()}円
+            </li>
+          );
+        })}
       </ul>
     </>
   );
