913667e0911816299ae8cd5d87a359a21574ba66
sss <sss@sss.com>
2026-04-22 15:36:29 +0900

    カテゴリの変更を1日1回に制限する機能を実装

--- src/service/categoryService.js
+++ src/service/categoryService.js
@@ -56,6 +56,9 @@ const STORAGE_KEY_ACTIVE = "my_categories_active";
 // 過去のカテゴリー
 const STORAGE_KEY_ARCHIVED = "my_categories_archived";
 
+// 変更を加えた日時を保存するキー
+const STORAGE_KEY_LAST_EDIT = "category_last_edit_time";
+
 /**
  * カテゴリ一覧を取得する
  * @returns {Array} カテゴリオブジェクトの配列 (LocalStorageが空なら初期値)
@@ -135,7 +138,7 @@ export const archivedCategoriesAtom = atom(getArchivedCategories());
  * @param {Array} activeCat - 画面用のActive配列
  * @param {Object} archivedCat - ArchivedのMap
  */
-export const saveAllCategories = (activeCat, archivedCat) => {
+export const saveAllCategories = (activeCat, archivedCat, editDate) => {
   const activeMap = toPureMap(activeCat);
 
   // archiveするものは空欄を除去
@@ -149,11 +152,16 @@ export const saveAllCategories = (activeCat, archivedCat) => {
   localStorage.setItem(STORAGE_KEY_ACTIVE, JSON.stringify(activeMap));
   localStorage.setItem(STORAGE_KEY_ARCHIVED, JSON.stringify(archivedMap));
 
+  // 変更を加えた日付を保存
+  localStorage.setItem(STORAGE_KEY_LAST_EDIT, JSON.stringify(editDate));
+
   // 保存されたカテゴリをコンソール表示
   console.log("activeMap:");
   console.table(activeMap);
   console.log("archivedMap:");
   console.table(archivedMap);
+  // 日時の確認用
+  console.log(editDate.toLocaleString());
 };
 
 /**
@@ -197,3 +205,27 @@ export const resolveCategoryById = (id, activeList, archiveList) => {
   }
   return null;
 };
+
+/**
+ * カテゴリの変更が可能かどうかを判定
+ * @param {*} today
+ * @returns boolean
+ */
+export const checkAlreadyEditCategory = (today) => {
+  const lastEditDateRaw = localStorage.getItem(STORAGE_KEY_LAST_EDIT);
+  // ローカルストレージにデータがなければ変更されたことがないため変更可
+  if (!lastEditDateRaw) return true;
+
+  const lastEditDate = new Date(JSON.parse(lastEditDateRaw));
+
+  // 日付が一致しなかったらtrueを返す
+  return today.toDateString() !== lastEditDate.toDateString();
+};
+
+/**
+ * TODO:開発用リセットボタンなので不要になったら消すこと
+ */
+export function resetLastEditDate() {
+  localStorage.removeItem(STORAGE_KEY_LAST_EDIT);
+  console.log("🛠️ カテゴリ変更制限をリセットしました");
+}
