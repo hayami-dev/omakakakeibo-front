7405da638d45678cc7c09cb17f724fd92cdf8d16
sss <sss@sss.com>
2026-04-24 10:57:47 +0900

    Homeへ目標金額を表示＋toLocaleString()に修正

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,10 +1,13 @@
 /* 月毎の合計金額、縦線グラフを表示する */
 
-export default function Summary({ total, selectMonth }) {
+export default function Summary({ total, selectMonth, monthlyBudget }) {
   return (
-    <p>
-      {selectMonth}月の合計：
-      <strong>{total.toLocaleString("ja-JP")}円</strong>
-    </p>
+    <>
+      <p>
+        {selectMonth}月の合計：
+        <strong>{total.toLocaleString()}円</strong>
+      </p>
+      <p>目標金額{monthlyBudget.toLocaleString()}円</p>
+    </>
   );
 }
