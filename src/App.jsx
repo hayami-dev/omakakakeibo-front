dc049745904212e9d75e05241af5195a862817d7
sss <sss@sss.com>
2026-05-12 13:30:40 +0900

    UseEffectでのfetchデータ取得をApp.jsxへ移動

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useSetAtom } from "jotai";
+// ルート
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
@@ -6,8 +9,26 @@ import User from "./pages/User";
 import CategoryEdit from "./pages/user/CategoryEdit";
 import Header from "./components/Header";
 import BudgetEdit from "./pages/user/BudgetEdit";
+// DBからカテゴリを取得
+import {
+  activeCategoriesAtom,
+  categoryService,
+} from "./service/categoryService";
 
 function App() {
+  const setActiveCategories = useSetAtom(activeCategoriesAtom);
+  useEffect(() => {
+    const loadData = async () => {
+      // JavaAPIを叩いて加工済みデータを取得
+      // userId：1
+      const data = await categoryService.fetchActiveCategories(1);
+
+      // 取得したデータをAtomに保存
+      setActiveCategories(data);
+    };
+    loadData();
+  }, [setActiveCategories]); // 第2引数を空にすると初回のみ実行になる
+
   return (
     <>
       <Header />
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,28 +1,11 @@
-import { useAtomValue, useSetAtom } from "jotai";
+import { useAtomValue } from "jotai";
 import { NavLink } from "react-router";
-import {
-  activeCategoriesAtom,
-  categoryService,
-} from "../service/categoryService";
-import { useEffect } from "react";
+import { activeCategoriesAtom } from "../service/categoryService";
 
 // ユーザー個別の情報の表示画面
 export default function User() {
   // カテゴリ一覧を取得
-  const setActiveCategories = useSetAtom(activeCategoriesAtom);
   const categories = useAtomValue(activeCategoriesAtom);
-
-  useEffect(() => {
-    const loadData = async () => {
-      // JavaAPIを叩いて加工済みデータを取得
-      // userId：1
-      const data = await categoryService.fetchActiveCategories(1);
-
-      // 取得したデータをAtomに保存
-      setActiveCategories(data);
-    };
-    loadData();
-  }, [setActiveCategories]); // 第2引数を空にすると初回のみ実行になる
 
   // 処理
   return (
